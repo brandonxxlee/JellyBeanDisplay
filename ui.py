861b5a043fa3a68d5892ccf16a10bddb1a3c85cc
Scott Numamoto <scott.numamoto@gmail.com>
2017-06-04 00:50:18 -0700

    Revise UI css

--- ui.py
+++ ui.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 import jellybean
 app = Flask(__name__)
 
+current_display = None
 test = {"richmond":["10", "20"], "irvine":["90"]}
 @app.route("/")
 def hello():
-    return render_template('display.html', trains=jellybean.bart_info())
+    try:
+        current_display = render_template('display.html', trains=jellybean.bart_info())
+    except:
+        pass
+    return current_display
 
 if __name__ == "__main__":
     app.run()
